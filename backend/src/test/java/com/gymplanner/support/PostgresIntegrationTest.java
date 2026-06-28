5e4e9b046491d5a8077c7a6d2c17bfdafeb5242e
Felipe Garcia <pipeg1069@gmail.com>
2026-06-28 19:06:52 -0300

    chore(testing): dirty context after class for postgres integration tests

--- backend/src/test/java/com/gymplanner/support/PostgresIntegrationTest.java
+++ backend/src/test/java/com/gymplanner/support/PostgresIntegrationTest.java
@@ -2,6 +2,7 @@ package com.gymplanner.support;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -12,6 +13,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @SpringBootTest
 @Testcontainers
 @ActiveProfiles("postgres-integration")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public abstract class PostgresIntegrationTest {
 
     @Container
